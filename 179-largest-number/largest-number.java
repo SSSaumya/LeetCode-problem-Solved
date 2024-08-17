be1c9a4e7b180e5b7adfd48c3f9c2027764de821
SAUMYA S <166050303+SSSaumya@users.noreply.github.com>
2024-08-17 12:11:10 +0530

    Time: 12 ms (8.44%) | Memory: 44.9 MB (5.32%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -4,30 +4,30 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             strNums[i] = String.valueOf(nums[i]);
         }
-
-        int n = strNums.length;
-        int ind = -1;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
+        for (int i = 0; i < strNums.length - 1; i++) {
+            for (int j = i + 1; j < strNums.length; j++) {
                 if (compare(strNums[i], strNums[j]) < 0) {
                     String temp = strNums[i];
                     strNums[i] = strNums[j];
                     strNums[j] = temp;
                 }
             }
-        }        
+        }
         if (strNums[0].equals("0")) {
             return "0";
         }
-        StringBuilder large = new StringBuilder();
+        String result = "";
         for (String num : strNums) {
-            large.append(num);
+            result += num; 
         }
-        return large.toString();
+        
+        return result;
     }
+
     public int compare(String a, String b) {
-        String order = a + b;
-        String orders = b + a;
-        return order.compareTo(orders);
+        String order1 = a + b;
+        String order2 = b + a;
+        return order1.compareTo(order2);
     }
 }
+
