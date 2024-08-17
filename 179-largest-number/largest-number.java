3f98a10e6bfa691e28a4182bb65adcda11bc2016
SAUMYA S <166050303+SSSaumya@users.noreply.github.com>
2024-08-17 12:05:52 +0530

    Time: 13 ms (6.80%) | Memory: 45 MB (5.32%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -1,4 +1,3 @@
-
 class Solution {
     public String largestNumber(int[] nums) {
         String[] strNums = new String[nums.length];
@@ -8,38 +7,27 @@ class Solution {
 
         int n = strNums.length;
         int ind = -1;
-
-        // Manually find the largest permutation by comparing and swapping
         for (int i = 0; i < n - 1; i++) {
             for (int j = i + 1; j < n; j++) {
-                // Compare the concatenated results of strNums[i] and strNums[j]
                 if (compare(strNums[i], strNums[j]) < 0) {
-                    // Swap strNums[i] and strNums[j]
                     String temp = strNums[i];
                     strNums[i] = strNums[j];
                     strNums[j] = temp;
                 }
             }
-        }
-
-        // Handle edge case where the array is all zeros
+        }        
         if (strNums[0].equals("0")) {
             return "0";
         }
-
-        // Concatenate all strings to form the final number
-        StringBuilder largestNumber = new StringBuilder();
+        StringBuilder large = new StringBuilder();
         for (String num : strNums) {
-            largestNumber.append(num);
+            large.append(num);
         }
-
-        return largestNumber.toString();
+        return large.toString();
     }
-
-    // Compare two strings by their concatenated results
-    private int compare(String a, String b) {
-        String order1 = a + b;
-        String order2 = b + a;
-        return order1.compareTo(order2);
+    public int compare(String a, String b) {
+        String order = a + b;
+        String orders = b + a;
+        return order.compareTo(orders);
     }
 }
